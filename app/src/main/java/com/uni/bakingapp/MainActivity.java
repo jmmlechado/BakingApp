3576025e5b37bdf256fa15e1583e7bdbbd477da9
jmmlechado <jmmlechado@gmail.com>
2018-06-20 13:43:41 +0200

    Setup Timber

--- app/src/main/java/com/uni/bakingapp/MainActivity.java
+++ app/src/main/java/com/uni/bakingapp/MainActivity.java
@@ -3,11 +3,19 @@ package com.uni.bakingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up Timber
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+        Timber.d("Timber up!");
     }
 }
