cc02eb133520c10c195e4f4009d292d0babcb405
jmmlechado <jmmlechado@gmail.com>
2018-06-20 14:27:21 +0200

    Setup Butterknife

--- app/src/main/java/com/uni/bakingapp/MainActivity.java
+++ app/src/main/java/com/uni/bakingapp/MainActivity.java
@@ -2,10 +2,16 @@ package com.uni.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_hello) TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
             Timber.plant(new Timber.DebugTree());
         }
         Timber.d("Timber up!");
+
+        // Do UI binding with Butterknife
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_hello)
+    public void test(){
+        mTextView.setText("Tested!");
     }
 }
